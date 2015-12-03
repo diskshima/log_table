a41dc95639370a9d28bdc99f7537295794c72473
Daisuke Shimamoto <diskshima@gmail.com>
2015-12-03 14:32:18 +0900

    Add triggers for on delete and add status outputs

--- lib/log_table.rb
+++ lib/log_table.rb
@@ -9,6 +9,31 @@ require 'log_table/version'
 require 'hairtrigger'
 
 module LogTable
+  module LogStatus
+    STATUS_COLUMN_NAME = 'log_table_status'
+    INSERTED = 'inserted'
+    UPDATED = 'updated'
+    DELETED = 'deleted'
+
+    def self.sql_func(status, model_name, column_names, table_name, options)
+      col_names = column_names
+
+      db_columns = [STATUS_COLUMN_NAME] + col_names.map { |col_name|
+          col_name == 'id' ? "#{model_name.underscore}_id" : "#{col_name}"
+        }
+
+      db_columns_str = db_columns.join(', ')
+
+      values = (["\"#{status}\""] + col_names.map { |col| "NEW.#{col}" }).join(', ')
+
+      log_table_name = options[:table_name] || "#{table_name}_log"
+
+      sql = "INSERT INTO #{log_table_name}(#{db_columns_str}) VALUES (#{values})"
+
+      sql
+    end
+  end
+
   def self.included(base)
     base.send :extend, ClassMethods
   end
@@ -23,24 +48,20 @@ module LogTable
     # - :table_name - Specify table name of the log table.
     #
     def add_log_trigger(options = {})
-      col_names = column_names
-
-      db_columns = col_names.map { |col_name|
-        col_name == 'id' ? "#{name.underscore}_id" : "#{col_name}"
-      }.join(', ')
-
-      values = col_names.map { |col| "NEW.#{col}" }.join(', ')
-
-      log_table_name = options[:table_name] || "#{table_name}_log"
-
-      insert_sql = "INSERT INTO #{log_table_name}(#{db_columns}) VALUES (#{values})"
 
       trigger.after(:insert) do
-        insert_sql
+        LogStatus.sql_func(LogStatus::INSERTED, name, column_names, table_name,
+          options)
       end
 
       trigger.after(:update) do
-        insert_sql
+        LogStatus.sql_func(LogStatus::UPDATED, name, column_names, table_name,
+          options)
+      end
+
+      trigger.after(:delete) do
+        LogStatus.sql_func(LogStatus::DELETED, name, column_names, table_name,
+          options)
       end
     end
   end
